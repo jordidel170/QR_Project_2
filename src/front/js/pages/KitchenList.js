f19a2b1bbea3a2708d57c2b571c11860661ecefe
Tu Nguyen <tuna7588@gmail.com>
2024-07-02 10:09:44 +0000

    date and hour added to order

--- src/front/js/pages/KitchenList.js
+++ src/front/js/pages/KitchenList.js
@@ -76,17 +76,17 @@ export const KitchenList = () => {
     return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
   };
 
-  // const formatDateTime = (timestamp) => {
-  //   const date = new Date(timestamp);
-  //   const options = {
-  //     year: "numeric",
-  //     month: "short",
-  //     day: "numeric",
-  //     hour: "2-digit",
-  //     minute: "2-digit",
-  //   };
-  //   return date.toLocaleDateString(undefined, options);
-  // };
+  const formatDateTime = (timestamp) => {
+    const date = new Date(timestamp);
+    const options = {
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+    };
+    return date.toLocaleDateString(undefined, options);
+  };
 
   const toggleItemCompleted = (orderId, itemId) => {
     setCompletedItems((prevCompletedItems) => ({
@@ -165,10 +165,11 @@ const toggleExpandOrder = (orderId) => {
                 <path d="M360-840v-80h240v80H360Zm80 440h80v-240h-80v240Zm40 320q-74 0-139.5-28.5T226-186q-49-49-77.5-114.5T120-440q0-74 28.5-139.5T226-694q49-49 114.5-77.5T480-800q62 0 119 20t107 58l56-56 56 56-56 56q38 50 58 107t20 119q0 74-28.5 139.5T734-186q-49 49-114.5 77.5T480-80Zm0-80q116 0 198-82t82-198q0-116-82-198t-198-82q-116 0-198 82t-82 198q0 116 82 198t198 82Zm0-280Z" />
               </svg>
               {formatTime(elapsedTime)}
-              {/* {formatDateTime(order.created_at)} */}
+              
             </p>
             </div>
             </div>
+            <p className='order-date'>{formatDateTime(order.created_at)}</p>
             <ul className="order-items-list">
               {order.order_items.map((item) => (
                 <li key={item.id} className={`order-item ${completedItems[order.id]?.[item.id] ? 'completed' : ''}`}>
