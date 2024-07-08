9c27bcc440bdf7b7df25b9f6c2bdd4409f3258d9
jordidel170 <jordidel170@gmail.com>
2024-07-08 23:48:00 +0100

    Qr funciona

--- src/front/js/pages/GenerateQR.js
+++ src/front/js/pages/GenerateQR.js
@@ -11,7 +11,7 @@ export const GenerateQR = () => {
     useEffect(() => {
         const fetchTables = async () => {
             try {
-                const response = await axios.get('${process.env.BACKEND_URL}/app/tables');
+                const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/app/tables`);
                 const sortedTables = response.data.sort((a, b) => a.table_number - b.table_number);
                 setTables(sortedTables);
             } catch (error) {
@@ -26,7 +26,7 @@ export const GenerateQR = () => {
         event.preventDefault();
 
         try {
-            const response = await fetch(`${process.env.BACKEND_URL}/app/restaurants/${restaurantId}/tables/${tableNumber}/generate_qr`, {
+            const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/app/restaurants/${restaurantId}/tables/${tableNumber}/generate_qr`, {
                 method: 'GET',
             });
 
